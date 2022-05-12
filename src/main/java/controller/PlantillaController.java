acc4d48e043ec2ffcc09ae55d22d1176888eb5e6
pdelam01 <47599125+pdelam01@users.noreply.github.com>
2022-05-12 19:12:46 +0200

    clase dia 12 mayo

--- src/main/java/controller/PlantillaController.java
+++ src/main/java/controller/PlantillaController.java
@@ -23,6 +23,7 @@ public class PlantillaController implements Serializable{
     public void checkLoggedUser() throws IOException{
         System.out.println("checklogged user ");
         if(FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("empleadoLogged")!=null){
+            System.out.println("estoy en el if()");
             FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getApplicationContextPath()+"/private/home2.xhtml");    
         }else{
             System.out.println("Lol yo en el else bro");
