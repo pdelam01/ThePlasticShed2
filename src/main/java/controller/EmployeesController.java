da47ab724690cf61e155e5a851ec7fb268339e50
dfernv04 <dfernv04@estudiantes.unileon.es>
2022-05-27 17:40:13 +0200

    commit previo a cambios

--- src/main/java/controller/EmployeesController.java
+++ src/main/java/controller/EmployeesController.java
@@ -7,11 +7,16 @@ package controller;
 
 import EJB.EmployeesFacadeLocal;
 import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.component.behavior.FacesBehavior;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import modelo.Employees;
@@ -26,13 +31,23 @@ import modelo.Employees;
 public class EmployeesController implements Serializable{
     
     private List<Employees> employeesList;
+    private Employees employee;
+    private String rol;
+    private Employees employeeEdit;
+    private String dni;
     
+    private List<SelectItem> roleList;
+            
     @EJB
     private EmployeesFacadeLocal employeesEJB;
     
     @PostConstruct
     public void init(){
         employeesList = loadEmployeesList();
+        employee = new Employees();
+        roleList = new ArrayList<SelectItem>();
+        addItems();
+        employee.setBirthday(new Date());
     }
 
     public List<Employees> loadEmployeesList() {
@@ -58,6 +73,27 @@ public class EmployeesController implements Serializable{
         }
     }
     
+    private void addItems() {
+        roleList.add(new SelectItem("Peón", "Peón"));
+        roleList.add(new SelectItem("Secretario", "Secretario"));
+        roleList.add(new SelectItem("Administrador", "Administrador"));
+    }
+    
+    public void addEmployees() {
+        System.out.println("U: "+ employee.getUsername()+employee.getNameEmp()+employee.getDni()+employee.getSsn()+employee.getPhoneNum()+rol);
+        employee.setRole(rol);
+        //employeesEJB.create(employee);
+    }
+    
+    public void searchEmployees() {
+        for(int i=0; i<employeesList.size(); i++) {
+            if(this.dni.equals(employeesList.get(i).getDni())) {
+                employeeEdit = employeesList.get(i);
+            }
+        }
+        System.out.println("Nombre: "+employeeEdit.getNameEmp());
+    }
+    
     public List<Employees> getEmployeesList(){
         return employeesList;
     }
@@ -65,4 +101,45 @@ public class EmployeesController implements Serializable{
     public void setEmployeesList(List<Employees> listEmp){
         employeesList = listEmp;
     }
+    
+    public Employees getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employees employee) {
+        this.employee = employee;
+    }
+    
+    public Employees getEmployeeEdit() {
+        return employeeEdit;
+    }
+
+    public void setEmployeeEdit(Employees employee) {
+        this.employeeEdit = employee;
+    }
+    
+    public List<SelectItem> getListRoles() {
+        return roleList;
+    }
+ 
+    public void setListaColores(List<SelectItem> roleList) {
+        this.roleList = roleList;
+    }
+    
+    public String getRol() {
+        return this.rol;
+    }
+    
+    public void setRol(String role) {
+        this.rol = role;
+    }
+    
+    public String getDni() {
+        return this.dni;
+    }
+    
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
 }
