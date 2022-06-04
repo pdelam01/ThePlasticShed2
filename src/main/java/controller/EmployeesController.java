79da143c28c942b3d3cad02326f618b5c48f3750
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2022-06-04 12:23:38 +0200

    Añadido mensaje success encontrar usuario

--- src/main/java/controller/EmployeesController.java
+++ src/main/java/controller/EmployeesController.java
@@ -25,11 +25,10 @@ import modelo.Employees;
  *
  * @author De la Hera
  */
-
 @Named
 @ViewScoped
-public class EmployeesController implements Serializable{
-    
+public class EmployeesController implements Serializable {
+
     private List<Employees> employeesList;
     private Employees employee;
     private String rol;
@@ -38,12 +37,12 @@ public class EmployeesController implements Serializable{
     private List<SelectItem> roleList;
     private Date date;
     private Date dateEdit;
-            
+
     @EJB
     private EmployeesFacadeLocal employeesEJB;
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         employeesList = loadEmployeesList();
         employee = new Employees();
         roleList = new ArrayList<SelectItem>();
@@ -61,57 +60,57 @@ public class EmployeesController implements Serializable{
             return null;
         }
     }
-    
-    public void removeEmployees(int id){
+
+    public void removeEmployees(int id) {
         try {
             Employees aux = getEmployee(id);
             employeesEJB.remove(aux);
-            FacesContext.getCurrentInstance().getExternalContext().redirect("usermng.xhtml"); 
+            FacesContext.getCurrentInstance().getExternalContext().redirect("usermng.xhtml");
         } catch (Exception e) {
-             System.out.println("Oh no! Algo ha ido mal: " + e.getMessage());
+            System.out.println("Oh no! Algo ha ido mal: " + e.getMessage());
         }
     }
-    
+
     private Employees getEmployee(int id) {
-        for(int i=0; i<employeesList.size(); i++) {
-            if(employeesList.get(i).getIdEmployee()==id) {
+        for (int i = 0; i < employeesList.size(); i++) {
+            if (employeesList.get(i).getIdEmployee() == id) {
                 return employeesList.get(i);
             }
         }
         return null;
     }
-    
+
     private void addItems() {
         roleList.add(new SelectItem("Peón", "Peón"));
         roleList.add(new SelectItem("Secretario", "Secretario"));
         roleList.add(new SelectItem("Administrador", "Administrador"));
     }
-    
+
     public void addEmployees() {
         boolean todoOk = true;
         if (!utils.Utils.validUsername(employee.getUsername())) {
             FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Username introducido erróneo, "
-                            + "se procede a dejar el almacenado en base de datos"));
+                    + "se procede a dejar el almacenado en base de datos"));
             todoOk = false;
         }
         if (!utils.Utils.validName(employee.getNameEmp())) {
             FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Nombre introducido erróneo, "
-                            + "se procede a dejar el almacenado en base de datos"));
+                    + "se procede a dejar el almacenado en base de datos"));
             todoOk = false;
         }
-        if(!utils.Utils.validDNI(employee.getDni())){
+        if (!utils.Utils.validDNI(employee.getDni())) {
             FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "DNI introducido erróneo, "
-                            + "se procede a dejar el almacenado en base de datos")); 
+                    + "se procede a dejar el almacenado en base de datos"));
             todoOk = false;
         }
-        if(!utils.Utils.validSSN(employee.getSsn())){
+        if (!utils.Utils.validSSN(employee.getSsn())) {
             FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "SSN introducido erróneo, "
-                            + "se procede a dejar el almacenado en base de datos")); 
+                    + "se procede a dejar el almacenado en base de datos"));
             todoOk = false;
         }
-        if(!utils.Utils.validPhoneNumber(employee.getPhoneNum())){
+        if (!utils.Utils.validPhoneNumber(employee.getPhoneNum())) {
             FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Número de teléfono introducido erróneo, "
-                            + "se procede a dejar el almacenado en base de datos"));
+                    + "se procede a dejar el almacenado en base de datos"));
             todoOk = false;
         }
         if (todoOk) {
@@ -124,81 +123,83 @@ public class EmployeesController implements Serializable{
                 employeesList = loadEmployeesList();
             } catch (Exception e) {
                 cleanValuesDuplicate();
-                FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error, elementos duplicados")); 
+                FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error, elementos duplicados"));
                 System.out.println("Oh no! Algo ha ido mal: ");
             }
         }
     }
-    
-    private void cleanValuesDuplicate(){
+
+    private void cleanValuesDuplicate() {
         employee = new Employees();
         rol = "Peón";
         date = null;
     }
-    
+
     public void redirectAdd() {
-        try{
+        try {
             FacesContext.getCurrentInstance().getExternalContext().redirect("usermng.xhtml");
         } catch (Exception e) {
             System.out.println("Oh no! Algo ha ido mal: " + e.getMessage());
         }
     }
-    
+
     public void searchEmployees() {
         try {
-            boolean todoOk=false;
-            for(int i=0; i<employeesList.size(); i++) {
-                if(this.dni.equals(employeesList.get(i).getDni())) {
-                    System.out.println("Culo "+employeesList.get(i).getDni());
+            boolean todoOk = false;
+            for (int i = 0; i < employeesList.size(); i++) {
+                if (this.dni.equals(employeesList.get(i).getDni())) {
+                    System.out.println("Culo " + employeesList.get(i).getDni());
                     employeeEdit = employeesList.get(i);
-                    todoOk=true;
+                    todoOk = true;
                 }
             }
-            if(!todoOk){
-                dni="";
+            if (!todoOk) {
+                dni = "";
                 cleanValuesDNIFalse();
-                FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "DNI introducido erróneo")); 
+                FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "DNI introducido erróneo"));
             } else {
                 dni = "";
                 rol = employeeEdit.getRole();
                 dateEdit = employeeEdit.getBirthdayDate();
+                FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Empleado encontrado correctamente"));
+
             }
         } catch (Exception e) {
             System.out.println("Oh no! Algo ha ido mal: " + e.getMessage());
         }
     }
-    
-    private void cleanValuesDNIFalse(){
+
+    private void cleanValuesDNIFalse() {
         employeeEdit = new Employees();
         rol = "Peón";
         dateEdit = null;
     }
-    
+
     public void updateEmployee() {
         boolean todoOk = true;
         if (!utils.Utils.validUsername(employeeEdit.getUsername())) {
             FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Username introducido erróneo, "
-                            + "no se modifica"));
+                    + "no se modifica"));
             todoOk = false;
         }
         if (!utils.Utils.validName(employeeEdit.getNameEmp())) {
             FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Nombre introducido erróneo, "
-                            + "no se modifica"));
+                    + "no se modifica"));
             todoOk = false;
         }
-        if(!utils.Utils.validDNI(employeeEdit.getDni())){
+        if (!utils.Utils.validDNI(employeeEdit.getDni())) {
             FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "DNI introducido erróneo, "
-                            + "no se modifica")); 
+                    + "no se modifica"));
             todoOk = false;
         }
-        if(!utils.Utils.validSSN(employeeEdit.getSsn())){
+        if (!utils.Utils.validSSN(employeeEdit.getSsn())) {
             FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "SSN introducido erróneo, "
-                            + "no se modifica")); 
+                    + "no se modifica"));
             todoOk = false;
         }
-        if(!utils.Utils.validPhoneNumber(employeeEdit.getPhoneNum())){
+        if (!utils.Utils.validPhoneNumber(employeeEdit.getPhoneNum())) {
             FacesContext.getCurrentInstance().addMessage("MessageId", new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Número de teléfono introducido erróneo, "
-                            + "no se modifica"));
+                    + "no se modifica"));
             todoOk = false;
         }
         if (todoOk) {
@@ -208,17 +209,17 @@ public class EmployeesController implements Serializable{
             employeesEJB.edit(employeeEdit);
             cleanValuesDNIFalse();
         }
-        
+
     }
-    
-    public List<Employees> getEmployeesList(){
+
+    public List<Employees> getEmployeesList() {
         return employeesList;
     }
-    
-    public void setEmployeesList(List<Employees> listEmp){
+
+    public void setEmployeesList(List<Employees> listEmp) {
         employeesList = listEmp;
     }
-    
+
     public Employees getEmployee() {
         return employee;
     }
@@ -226,7 +227,7 @@ public class EmployeesController implements Serializable{
     public void setEmployee(Employees employee) {
         this.employee = employee;
     }
-    
+
     public Employees getEmployeeEdit() {
         return employeeEdit;
     }
@@ -234,31 +235,31 @@ public class EmployeesController implements Serializable{
     public void setEmployeeEdit(Employees employee) {
         this.employeeEdit = employee;
     }
-    
+
     public List<SelectItem> getListRoles() {
         return roleList;
     }
- 
+
     public void setListaColores(List<SelectItem> roleList) {
         this.roleList = roleList;
     }
-    
+
     public String getRol() {
         return this.rol;
     }
-    
+
     public void setRol(String role) {
         this.rol = role;
     }
-    
+
     public String getDni() {
         return this.dni;
     }
-    
+
     public void setDni(String dni) {
         this.dni = dni;
     }
-    
+
     public Date getDate2() {
         return date;
     }
@@ -274,6 +275,5 @@ public class EmployeesController implements Serializable{
     public void setDateEdit(Date dateEdit) {
         this.dateEdit = dateEdit;
     }
-    
 
 }
