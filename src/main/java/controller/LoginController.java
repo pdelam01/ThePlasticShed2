2a2fb340a6e6e3329483d2255a3d8d0d5125ef7e
dfernv04 <dfernv04@estudiantes.unileon.es>
2022-06-04 13:27:37 +0200

    Login arreglado

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -40,31 +41,34 @@ public class LoginController implements Serializable {
     public void checkCredentials() {
         try {
             List result = employeesEJB.loginCredentials(employees.getUsername());
-            System.out.println("Hola");
             if (!result.isEmpty()) {
-                System.out.println("Hola1");
                 if(verifyPassword((Employees) result.get(0))) {
                     System.out.println(result.get(0).toString());
                     FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("empleadoLogged", result.get(0));
                     FacesContext.getCurrentInstance().getExternalContext().redirect("faces/private/home.xhtml");
                 } else {
-                    System.out.println("Hola2");
-                    System.out.println("USUARIO O CONTRASEÑA INCORRECTA");
-                    FacesContext.getCurrentInstance().getExternalContext().redirect("public/error404.xhtml");
+                    errorLogin();
                 }
             } else {
-                System.out.println("Hola3");
-                System.out.println("USUARIO O CONTRASEÑA INCORRECTA");
-                FacesContext.getCurrentInstance().getExternalContext().redirect("public/error404.xhtml");
+                errorLogin();
             }
         } catch (Exception e) {
-            System.out.println("Hola4");
-            System.out.println("controller.LoginController.checkCredentials Ha lledago " + e.getMessage());
+            errorLogin();
         }
     }
     
     private boolean verifyPassword(Employees emp) {
         return PasswordUtils.verifyUserPassword(employees.getPass(), emp.getPass(), emp.getSalt());
+    }
+    
+    private void errorLogin() {
+        cleanValues();
+        FacesContext.getCurrentInstance().addMessage("MessageId2", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Usuario o"
+            + " contraseña incorrecta"));
+    }
+    
+    private void cleanValues() {
+        employees = new Employees();
     }
 
     public void closeSession() {
